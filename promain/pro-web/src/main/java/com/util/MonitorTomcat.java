e31860302954a72c49dbab0aa2d9e57da467f012
Puffin86 <511635157@qq.com>
2015-06-17 11:14:18 +0800

    tomcat-monitor

---
+++ promain/pro-web/src/main/java/com/util/MonitorTomcat.java
@@ -0,0 +1,152 @@
+package com.util;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL; 
+import java.net.URLConnection; 
+ 
+import java.util.Date; 
+
+public class MonitorTomcat {
+	    private void keepTomcatAlive() throws NullPointerException { 
+	       
+	       String t = new String("mytomcat.exe"); 
+	       boolean isTomcatAlive = false; 
+	       java.io.BufferedReader in; 
+	       String s; 
+	       try { 
+	           URL url = new URL("http://localhost:18062/simpleweb/"); 
+	           URLConnection con = url.openConnection(); 
+	           in = new BufferedReader(new InputStreamReader(con.getInputStream())); 
+	           con.setConnectTimeout(5000); 
+	           con.setReadTimeout(10000); 
+	           while ((s = in.readLine()) != null) {
+	        	   System.out.println(s);
+//	              if (s.length() > 0) {
+//	                  // 如果能够读取到页面则证明可用 
+//	                  break; 
+//	              } 
+	           } 
+	           in.close(); 
+	       } catch (Exception ex) { 
+	    	   ex.printStackTrace();
+	       } 
+	 
+	       //判断进程中是否含有该tomcat进程
+	       //由于绿色版本的tomcat进程统一名称是jave.exe,可以通过修改进程名来识别，修改方法参考
+//	    1、window平台：
+//	     　　打开tomcat_home\bin\setclasspath.bat文件，找到set _RUNJAVA="%JRE_HOME%\bin\java"这一行。
+//	     　　将该行注释掉 ,然后在该行下面添加如下两行并进行保存：
+//	     　　copy "%JAVA_HOME%\bin\java.exe" "%JAVA_HOME%\bin\my_java.exe"
+//	     　 set _RUNJAVA="%JAVA_HOME%\bin\my_java"
+//	     　　注意：my_java为你所想看到的进程名字（即在任务管理器中的映像名称),其实就是拷贝一份java.exe文件,并调用拷贝的java.exe
+//	     　　重启tomcat(用startup.bat启动)后在在任务管理器中看见的进程名就不再是java.exe,而是my_java.exe了。
+//	     　　2、linux/unix平台：
+//	     　　打开tomcat_home\bin\setclasspath.sh文件,找到_RUNJAVA="$JRE_HOME"/bin/java这一行,
+//	     　　并注释掉,然后在该行下面添加如两行并进行保存：
+//	     　　cp "$JAVA_HOME/bin/java" "$JAVA_HOME/bin/my_java"
+//	     _RUNJAVA="$JRE_HOME/bin/my_java"
+//	     　　重启tomcat,在终端输入ps -ef|grep my_java 进行查看
+	       try { 
+	           java.lang.Process p = Runtime.getRuntime().exec("tasklist"); 
+	           in = new BufferedReader(new InputStreamReader(p.getInputStream())); 
+	           
+	           while ((s = in.readLine()) != null) { 
+	        	   System.out.println(s);
+	              if (s.startsWith(t)) { 
+	                  isTomcatAlive = true; 
+	                  break; 
+	              } 
+	           } 
+	           in.close(); 
+	       } catch (Exception e) { 
+	           e.printStackTrace(); 
+	       } 
+	 
+	       if (isTomcatAlive) { 
+	           System.out.println("<" + new Date()+ "> Tomcat is alive but not response!"); 
+	           stopTomcat(); 
+	       } 
+//	       startTomcat(); 
+	    } 
+	 
+	  
+	    public void startTomcat() {
+	    	Tomcat tom = new Tomcat();
+	    	tom.startBatByProcess();
+			
+	    }
+	 
+	    public void stopTomcat() { 
+	    	Tomcat tom = new Tomcat();
+	    	tom.stopBatByprocess();
+	    } 
+	 
+	  
+	 
+//	    public static void startTomcat() { 
+//	 
+//	       try { 
+//	 
+//	           java.lang.Process p = java.lang.Runtime.getRuntime().exec("net stop \"Apache Tomcat\""); 
+//	 
+//	       } catch (Exception e) { 
+//	 
+//	           e.printStackTrace(); 
+//	 
+//	       } 
+//	 
+//	       try { 
+//	 
+//	           java.lang.Process p = java.lang.Runtime.getRuntime().exec( 
+//	 
+//	                  "net start \"Apache Tomcat\""); 
+//	 
+//	           java.io.BufferedReader in = new java.io.BufferedReader( 
+//	 
+//	                  new java.io.InputStreamReader(p.getInputStream())); 
+//	 
+//	           String s; 
+//	 
+//	           String t = "启动成功"; 
+//	 
+//	           boolean restart = false; 
+//	 
+//	           while ((s = in.readLine()) != null) { 
+//	 
+//	              if (s.indexOf(t) != -1) { 
+//	 
+//	                  restart = true; 
+//	 
+//	                  break; 
+//	 
+//	              } 
+//	 
+//	           } 
+//	 
+//	           System.out.println("<" + new Date() + "> Tomcat is start " 
+//	 
+//	                  + (restart ? "OK" : "ERROR")); 
+//	 
+//	       } catch (Exception e) { 
+//	 
+//	           e.printStackTrace(); 
+//	 
+//	       } 
+//	 
+//	    } 
+	 
+	  
+	 
+	    public static void main(String[] args) { 
+//	       while (true) { 
+	           try { 
+	        	   MonitorTomcat mt = new MonitorTomcat();
+	        	   mt.keepTomcatAlive(); 
+//	              Thread.sleep(30000); 
+	           } catch (Exception ex) {
+	        	   ex.printStackTrace();
+	           } 
+//	       } 
+	    }
+}
--- promain/pro-web/src/main/java/com/util/Tomcat.java
+++ promain/pro-web/src/main/java/com/util/Tomcat.java
@@ -1,4 +1,4 @@
-package com.tomcat;
+package com.util;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -6,35 +6,37 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 
 public class Tomcat {
-	public void  CmdExec(String cmdline) { 
-		try { 
-			String line; 
-			Process p = Runtime.getRuntime().exec(cmdline); 
-			BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream())); 
-			while ((line = input.readLine()) != null) { 
-				System.out.println(line); 
-			} 
-			input.close(); 
-		} catch (Exception err) { 
-			err.printStackTrace(); 
-		}
-	} 
-
 		public static void main(String argv[]) {
 			Tomcat tom = new Tomcat();
-//			cmd.exe /c D:\\apache-tomcat-6.0.18\\bin\\startup.bat
-//			tom.CmdExec("cmd.exe /c D:\\nginx+tomcat+redis\\nginx-tomcatv7-18062\\bin\\startup.bat");
-//			tom.CmdExec("cmd.exe /k start D:\\nginx+tomcat+redis\\nginx-tomcatv7-18062\\bin\\startup.bat");
-//			tom.CmdExec("cmd.exe /c start D:/nginx+tomcat+redis/nginx-tomcatv7-18062/bin/startup.bat");
-//			tom.CmdExec("cmd.exe /c dir");
-			System.out.println("end start");
-			
-			tom.testbat();
-			
+			tom.startBatByProcess();
+//			tom.stopBatByprocess();
 		} 
 		
+		public void stopBatByprocess (){
+			try{
+				ProcessBuilder builder = new ProcessBuilder ("cmd", "/c", "shutdown.bat"); 
+				 builder.directory (new File ("D:/nginx+tomcat+redis/nginx-tomcatv7-18062/bin/")); 
+				 Process process = builder.start (); 
+				 InputStream is = process.getInputStream (); 
+				 InputStreamReader isr = new InputStreamReader (is, "GBK"); 
+				 BufferedReader br = new BufferedReader (isr); 
+				 String line; 
+//				 process.destroy();
+				 while (( line = br.readLine () ) != null) 
+				 {
+					 System.out.println (line); 
+				 }
+//				 process.destroy();
+				 
+				 System.out.println("stop success~~~");
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+			 
+		 } 
 		
-		public void testbat (){
+		
+		public void startBatByProcess (){
 			try{
 				ProcessBuilder builder = new ProcessBuilder ("cmd", "/c", "startup.bat"); 
 				 builder.directory (new File ("D:/nginx+tomcat+redis/nginx-tomcatv7-18062/bin/")); 
